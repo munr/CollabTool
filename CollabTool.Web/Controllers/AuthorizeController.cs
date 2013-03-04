25bfd80eb0eac043ff443c574ac2121ca1d9dfbb
Munsifali Rashid <mun.rashid@gmail.com>
2013-03-04 18:01:27 -0500

    Project Skeleton

---
+++ CollabTool.Web/Controllers/AuthorizeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace CollabTool.Web.Controllers
+{
+    public class AuthorizeController : Controller
+    {
+        public ActionResult Index(string code)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
