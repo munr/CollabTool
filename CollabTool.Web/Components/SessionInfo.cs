1ea5861f115b1141eb4c78e92641c9b2e30b21d3
Munsifali Rashid <mun.rashid@gmail.com>
2013-03-04 18:31:17 -0500

    Implemented basic authentication against inBloom sandbox

---
+++ CollabTool.Web/Components/SessionInfo.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Diagnostics;
+using System.Web;
+using System.Web.SessionState;
+
+
+namespace CollabTool.Web
+{
+	/// <summary>
+	/// This class is used for storing all information which needs to be 
+	/// maintained at Session Scope. If more items need to be stored in 
+	/// Session later in the project, add a property to this class.
+	/// </summary>
+	[Serializable]
+	public class SessionInfo
+	{
+		public const string SessionKey = "SessionInfo";
+
+		#region Accessors
+
+		public string UserId { get; set; }
+		public string AccessToken { get; set; }
+		public string FullName { get; set; }
+		public string Roles { get; set; }
+
+		#endregion
+
+		#region Static Accessors
+
+		/// <summary>
+		/// Returns a SessionInfo instance for the current user session
+		/// </summary>
+		/// 
+		public static SessionInfo Current
+		{
+			get
+			{
+				if (HttpContext.Current == null || HttpContext.Current.Session == null)
+				{
+					Debug.WriteLine("SessionInfo.Current : No HttpContext, so session info will not be saved");
+					return new SessionInfo();
+				}
+
+				return ForSession(HttpContext.Current.Session);
+			}
+		}
+
+		public static SessionInfo Empty
+		{
+			get
+			{
+				return new SessionInfo();
+			}
+		}
+
+		#endregion
+
+		/// <summary>
+		/// Reset all session settings to default values
+		/// </summary>
+		public void Reset()
+		{
+			UserId = string.Empty;
+			AccessToken = string.Empty;
+			FullName = string.Empty;
+			Roles = string.Empty;
+		}
+
+		#region Helper Methods
+
+		// Instances of SessionInfo should only be created by 
+		// the Current property, so make the constructor private
+		private SessionInfo()
+		{
+			Reset();
+		}
+
+		/// <summary>
+		/// Gets the SessionInfo for the specified session
+		/// </summary>
+		public static SessionInfo ForSession(HttpSessionState session)
+		{
+			var sessionInfo = session[SessionKey] as SessionInfo;
+			if (sessionInfo == null)
+			{
+				sessionInfo = new SessionInfo();
+				session.Add(SessionKey, sessionInfo);
+			}
+			return sessionInfo;
+		}
+
+		#endregion
+	}
+}
--- CollabTool.Web/Controllers/AuthorizeController.cs
+++ CollabTool.Web/Controllers/AuthorizeController.cs
@@ -1,12 +1,46 @@
 ﻿using System.Web.Mvc;
+using inBloomApiLibrary;
 
 namespace CollabTool.Web.Controllers
 {
-    public class AuthorizeController : Controller
-    {
-        public ActionResult Index(string code)
-        {
-            return RedirectToAction("Index", "Home");
-        }
-    }
+	public class AuthorizeController : Controller
+	{
+		public ActionResult Index(string code)
+		{
+			// Check for a token in the session already, and if found, no action is required
+			if (!string.IsNullOrEmpty(SessionInfo.Current.AccessToken))
+				return RedirectToAction("Index", "Home");
+
+			// Init oAuth
+			var oAuth = new OAuth();
+
+			// We get a code back from the first leg of OAuth process.  If we don't have one, let's get it.
+			if (string.IsNullOrEmpty(code))
+			{
+				string authorizationUrl = oAuth.GetAuthorizationUrl();
+				return Redirect(authorizationUrl);
+			}
+
+			// Otherwise, we have a code, we can run the second leg of OAuth process.
+			string authorization = oAuth.CallAuthorization(null, code);
+
+			// OAuth successful so get values, store in session and continue
+			if (authorization == "OAuthSuccess")
+			{
+				// Ensure that all required values were retrieved from the OAuth login
+				if (oAuth.AccessToken != null && oAuth.UserFullName != null && oAuth.UserSLIRoles != null && oAuth.UserId != null)
+				{
+					// Authorization successful; set session variables
+					SessionInfo.Current.AccessToken = oAuth.AccessToken;
+					SessionInfo.Current.FullName = oAuth.UserFullName;
+					SessionInfo.Current.Roles = oAuth.UserSLIRoles;
+					SessionInfo.Current.UserId = oAuth.UserId;
+
+					return RedirectToAction("Index", "Home");
+				}
+			}
+
+			return Content("Error authorizing");
+		}
+	}
 }
