6236460d3f4bf35ff394d69c8a97fb4772cb53e5
Munsifali Rashid <mun.rashid@gmail.com>
2013-03-05 14:13:07 -0500

    Simplified route configuration

--- CollabTool.Web/App_Start/RouteConfig.cs
+++ CollabTool.Web/App_Start/RouteConfig.cs
@@ -9,6 +9,12 @@ namespace CollabTool.Web
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+			routes.MapRoute(
+				name: "Api",
+				url: "api/{action}/{studentId}",
+				defaults: new { controller = "Api" }
+			);
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
