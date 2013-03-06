6e100d3546a9020b7fbef3d7caa6925c1b1e0c51
Munsifali Rashid <mun.rashid@gmail.com>
2013-03-06 14:31:17 -0500

    Changed default route to point to CollabTool instead of Index

--- CollabTool.Web/App_Start/RouteConfig.cs
+++ CollabTool.Web/App_Start/RouteConfig.cs
@@ -21,10 +21,12 @@ namespace CollabTool.Web
 				defaults: new { controller = "Api" }
 			);
 
+			// Default page
+			
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "CollabTool", id = UrlParameter.Optional }
             );
         }
     }
